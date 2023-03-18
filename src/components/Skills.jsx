616113ddf0d9e181c5682649404e88e5877b407f
luket8032 <66449324+luket8032@users.noreply.github.com>
2023-03-18 14:19:29 -0500

    added cards and titles
    projects implemented at later date

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -7,12 +7,12 @@ const Skills = () => {
     <div className='font-poppins text-white h-fit my-[10%]' id='skills'>
         <h1 className='text-5xl text-center my-[5%]'> My Skills </h1>
         <div className='grid grid-rows-2 grid-cols-3 mx-[10%] gap-16 justify-items-center p-8 text-lg' id='skills grid'>
-            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-lg'><SiJavascript size={140} className='mb-6'/> JavaScript</div>
-            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-lg'><SiPython size={140} className='mb-6'/>Python</div>
-            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-lg'><SiReact size={140} className='mb-6'/>React</div>
-            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-lg'><SiVuedotjs size={140} className='mb-6'/>VueJS</div>
-            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-lg'><BsFiletypeSql size={140} className='mb-6'/>SQL</div>
-            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-lg'><SiTailwindcss size={140} className='mb-6'/>Tailwind</div>
+            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-xl'><SiJavascript size={140} className='mb-6'/> JavaScript</div>
+            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-xl'><SiPython size={140} className='mb-6'/>Python</div>
+            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-xl'><SiReact size={140} className='mb-6'/>React</div>
+            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-xl'><SiVuedotjs size={140} className='mb-6'/>VueJS</div>
+            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-xl'><BsFiletypeSql size={140} className='mb-6'/>SQL</div>
+            <div className='bg-neutral-700 text-center p-8 rounded-xl shadow-xl'><SiTailwindcss size={140} className='mb-6'/>Tailwind</div>
         </div>
     </div>
   )
