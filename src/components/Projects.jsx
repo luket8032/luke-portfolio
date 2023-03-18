fe64576df4e6c5a4eaecb4a8db63e79e20586dab
luket8032 <66449324+luket8032@users.noreply.github.com>
2023-03-18 13:52:33 -0500

    Added title to project div

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Projects = () => {
   return (
     <div className='font-poppins text-white h-[600px] mx-[5%] p-16 bg-neutral-700 rounded-3xl' id='projects'>
-
+        <h1 className='text-5xl text-center'>My Projects</h1>
     </div>
   )
 }
