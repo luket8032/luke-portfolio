5bd8c711c72709942088fb21df2a17f1b1ecf572
luket8032 <luket8032@hotmail.com>
2023-05-21 15:54:57 -0500

    finished project component

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,21 +1,37 @@
 import React from 'react'
+import Cat from '../assets/cathub.PNG'
+import Portfolio from '../assets/portfolio.PNG'
 import { AnimationOnScroll } from 'react-animation-on-scroll';
+import { SiGithub } from 'react-icons/si';
+import { CgWebsite } from 'react-icons/cg';
 
 const Projects = () => {
   return (
     <AnimationOnScroll animateIn='animate__fadeInUp'> 
-        <div className='font-poppins text-white h-[600px] mx-[5%] p-16 bg-neutral-700 rounded-3xl shadow-xl' id='projects'>
+        <div className='font-poppins text-white h-fit mx-[5%] p-16 bg-neutral-700 rounded-3xl shadow-xl' id='projects'>
             <h1 className='text-5xl text-center'>My Projects</h1>
             <div className='flex group justify-center gap-36 my-10' id='project cards'>
-            <div className='h-96 w-72 bg-neutral-600 rounded-2xl shadow-xl'>
-                    <h1 className='text-2xl p-4 text-center'>PokeMart</h1>
+
+                <div className='h-fit w-72 bg-neutral-600 rounded-2xl shadow-xl p-4 flex-col'>
+                    <img src={Cat} alt="catimg"  className='rounded-md'/>
+                    <h1 className='text-2xl p-4 text-center leading-3 font-semibold'>CatHub</h1>
+                    <p className='text-center'>CatHub is a a web app built using React and The Cat API. CatHub provides a search tool and downloads for images of cats. CatHub also gives information of different cat breeds!</p>
+                    <div id='project-btns' className='flex justify-center gap-5'>
+                        <a href="https://cat-app-luket8032.vercel.app/" className='my-4 h-fit w-20 p-2 flex justify-center bg-blue-500 hover:bg-blue-700 rounded-md transition ease-in-out hover:scale-110 hover:-translate-y-1' ><CgWebsite size={25}/></a>
+                        <a href="http://localhost:3000/" className='my-4 h-fit w-20 p-2 flex justify-center bg-blue-500 hover:bg-blue-700 rounded-md transition ease-in-out hover:scale-110 hover:-translate-y-1' ><SiGithub size={25}/></a>
+                    </div>
                 </div>
-                <div className='h-96 w-72 bg-neutral-600 rounded-2xl shadow-xl'>
-                    <h1 className='text-2xl p-4 text-center'>Weather App</h1>
+
+                <div className='h-fit w-72 bg-neutral-600 rounded-2xl shadow-xl p-4'>
+                    <img src={Portfolio} alt="portfolio"  className='rounded-md border-2'/>
+                    <h1 className='text-2xl p-4 text-center font-semibold leading-3'>Portfolio Website</h1>
+                    <p className='text-center'>My portfolio website highlights my front-end development skills and user-centric approach. I create visually appealing designs and responsive layouts using HTML and Tailwind CSS</p>
+                    <div id='project-btns' className='flex justify-center gap-5'>
+                        <a href="http://localhost:3000/" className='my-4 h-fit w-20 p-2 flex justify-center bg-blue-500 hover:bg-blue-700 rounded-md transition ease-in-out hover:scale-110 hover:-translate-y-1' ><CgWebsite size={25}/></a>
+                        <a href="http://localhost:3000/" className='my-4 h-fit w-20 p-2 flex justify-center bg-blue-500 hover:bg-blue-700 rounded-md transition ease-in-out hover:scale-110 hover:-translate-y-1' ><SiGithub size={25}/></a>
+                    </div>
                 </div>
-                <div className='h-96 w-72 bg-neutral-600 rounded-2xl shadow-xl'>
-                    <h1 className='text-2xl p-4 text-center'>Lennin Repizo</h1>
-                </div>
+                
             </div>
         </div>
     </AnimationOnScroll>
