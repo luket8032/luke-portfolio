66492f4afeba88a4f2644574d92a7fd0f72e9d59
luket8032 <luket8032@hotmail.com>
2023-05-21 17:07:59 -0500

    changed resume

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,16 +1,9 @@
 import React from 'react'
 import pfp from '../assets/memoji_pfp.png'
-import resume from '../assets/resume.docx';
 
 const Hero = () => {
-  const downloadResume = () => {
-    const filename = "resume.docx";
-    const link = document.createElement("a");
-    link.href = resume;
-    link.download = filename;
-    document.body.appendChild(link);
-    link.click();
-    document.body.removeChild(link);
+  const redirect = () => {
+    window.location.href = 'https://docs.google.com/document/d/1B_3gikBYAIstc8lQ3W1frK7Z4btabDeY/edit?usp=sharing&ouid=107197544104944937436&rtpof=true&sd=true'
   }
 
   return (
@@ -20,7 +13,7 @@ const Hero = () => {
             <h1 className='text-6xl p-1'>Luke Tran</h1>
             <h1 className='text-3xl p-1'>Web Developer</h1>
             <p className='text-sm mt-10'>I'm a recent college graduate looking to start a career in web development.</p>
-            <button className='my-4 h-10 w-36 bg-blue-500 hover:bg-blue-700 rounded-md transition ease-in-out hover:scale-110 hover:-translate-y-1' onClick={downloadResume}>Resumé</button>
+            <button className='my-4 h-10 w-36 bg-blue-500 hover:bg-blue-700 rounded-md transition ease-in-out hover:scale-110 hover:-translate-y-1' onClick={redirect}>Resumé</button>
         </div> 
         <img class='float-right mr-[5%] h-[500px] w-[500px]' src={pfp} alt='profile pic'></img>
     </div>
